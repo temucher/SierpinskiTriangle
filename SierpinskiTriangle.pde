7038cb97deb95ada939f410eed46e5434bd74cdc
temucher <tjsmuch@gmail.com>
2015-01-21 11:36:42 -0800

    first commit

--- SierpinskiTriangle.pde
+++ SierpinskiTriangle.pde
@@ -1,16 +1,31 @@
+public int sX, sY;
 public void setup()
 {
-
+	size(500,500);
+	frameRate(5);
 }
 public void draw()
 {
-
+	sierpinski(sX, sY, 400);
 }
 public void mouseDragged()//optional
 {
-
+	sX = pmouseX;
+	sY = pmouseY;
 }
 public void sierpinski(int x, int y, int len) 
 {
-
+	if(len>=20) {
+		sierpinski(x,y,len/2);
+		sierpinski(x+len/2, y, len/2);
+		sierpinski(x+len/4, y-len/2, len/2);
+	}
+	else {
+		int r = (int)(Math.random()*255);
+		int g = (int)(Math.random()*255);
+		int b = (int)(Math.random()*255);
+		fill(r,g,b);
+		noStroke();
+		triangle(x, y, x+len/2, y-len, x+len, y);
+	}
 }
---
+++ build-tmp/source/SierpinskiTriangle.java
@@ -0,0 +1,56 @@
+import processing.core.*; 
+import processing.data.*; 
+import processing.event.*; 
+import processing.opengl.*; 
+
+import java.util.HashMap; 
+import java.util.ArrayList; 
+import java.io.File; 
+import java.io.BufferedReader; 
+import java.io.PrintWriter; 
+import java.io.InputStream; 
+import java.io.OutputStream; 
+import java.io.IOException; 
+
+public class SierpinskiTriangle extends PApplet {
+
+public int sX, sY;
+public void setup()
+{
+	size(500,500);
+	frameRate(5);
+}
+public void draw()
+{
+	sierpinski(sX, sY, 400);
+}
+public void mouseDragged()//optional
+{
+	sX = pmouseX;
+	sY = pmouseY;
+}
+public void sierpinski(int x, int y, int len) 
+{
+	if(len>=20) {
+		sierpinski(x,y,len/2);
+		sierpinski(x+len/2, y, len/2);
+		sierpinski(x+len/4, y-len/2, len/2);
+	}
+	else {
+		int r = (int)(Math.random()*255);
+		int g = (int)(Math.random()*255);
+		int b = (int)(Math.random()*255);
+		fill(r,g,b);
+		noStroke();
+		triangle(x, y, x+len/2, y-len, x+len, y);
+	}
+}
+  static public void main(String[] passedArgs) {
+    String[] appletArgs = new String[] { "SierpinskiTriangle" };
+    if (passedArgs != null) {
+      PApplet.main(concat(appletArgs, passedArgs));
+    } else {
+      PApplet.main(appletArgs);
+    }
+  }
+}
